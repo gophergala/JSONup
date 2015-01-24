b4c84356a77bc61a9193e5c0d7beac0c5974a5da
Eaden <_@eaden.net>
2015-01-24 14:07:33 +1100

    use gorilla/mux so can split off push endpoint

--- jsonup.go
+++ jsonup.go
@@ -4,12 +4,31 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 var listenAddr = flag.String("listenAddr", ":8080", "Web server listen address")
 
+//
+// func pushEndpoint(http.ResponseWriter, *http.Request) {
+//
+// }
+
 func main() {
 	flag.Parse()
+
+	router := mux.NewRouter()
+
+	// Push endpoint
+	// router.HandleFunc("/push/{userId}", pushEndpoint).Methods("POST")
+
+	// Static public files
+	publicFiles := http.FileServer(http.Dir("public"))
+	router.Handle("/", publicFiles)
+
+	// Start Web Server
+	http.Handle("/", router)
 	log.Println("Web server Listening on " + *listenAddr)
-	http.ListenAndServe(*listenAddr, http.FileServer(http.Dir("./public")))
+	http.ListenAndServe(*listenAddr, nil)
 }
